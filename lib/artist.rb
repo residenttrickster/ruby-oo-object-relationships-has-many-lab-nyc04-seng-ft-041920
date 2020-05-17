9fd01606655fc4bc299469357f7c5ec42c5c2878
residenttrickster <metkusblackus@gmail.com>
2020-05-17 13:41:41 -0400

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,27 @@
+require_relative 'song.rb'
+
+class Artist
+    
+    attr_accessor :name
+
+    def initialize(name)
+        @name = name
+    end
+
+    def add_song(song)
+        song.artist = self
+    end
+
+    def songs 
+        Song.all
+    end
+
+    def add_song_by_name(song_name)
+        song = Song.new(song_name)
+        song.artist = self
+    end
+
+    def self.song_count
+        Song.all.length
+    end
+end
